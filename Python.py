12bf984415171166549fd4e8e04df7bcb16fc5a2
Jettikis <77246838+Jettikis@users.noreply.github.com>
2021-04-23 20:04:24 -0400

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,13 +1,21 @@
-seasons = ['spring','summer','fall',"winter"]
-two_season_crop = ['summer','fall']
+#Created some lists to help my code
+
+seasons = ['spring','summer','fall','winter']
+spring = ['spring']
+summer = ['summer']
+fall = ['fall']
+winter = ['winter']
+two_seasons = ['summer','fall']
 ancient_seeds = ['spring','summer','fall']
+two_season_crops = ['corn','wheat','sunflower']
+spring_crops = ['blue jazz','cauliflower','garlic','green bean','kale','parsnip','potato','rhubarb','strawberry','tulip','unmilled rice']
 
 
 #Creating function to calculate plant growth time for corn, wheat and sunflowers because they are able to be grown in Summer and Fall which is unique
 
 def corn_wheat_sunflower_harvest():
     seed = input("Which seed are you planting? ")
-    if seed.lower() == "corn":
+    if seed.lower() in two_season_crops:
         print("You are planting",seed)
         season = input("Which season is it currently? ")
         if season.lower() in two_season_crop:
@@ -24,3 +32,16 @@ def corn_wheat_sunflower_harvest():
 
 
 corn_wheat_sunflower_harvest()
+
+
+def spring_harvest(): 
+    seed = input("Which seed are you planting? ")
+    if seed.lower() in spring_crops: 
+        print("You are planting",seed,"for the spring season."
+        season = input("Which season is it currently? ")
+        if season.lower() in spring:
+              print("This crop is in season!")
+              day = input("What day of the season is it? ")
+        else:
+              print("This crop is not is season currently. ")
+        
